0556244da7ce1ab010294b97da6da8ff95721d3f
Yassine Hadj messaoud <modepadu95@riseup.net>
2023-12-13 21:29:37 +0100

    Create router

---
+++ public/delivery/router.go
@@ -0,0 +1,15 @@
+package delivery
+
+import "github.com/gorilla/mux"
+
+func NewRouter(handler *UserHandler) *mux.Router {
+	r := mux.NewRouter()
+
+	r.HandleFunc("/users/register", handler.RegisterHandler).Methods("POST")
+	r.HandleFunc("/users/login", handler.LoginHandler).Methods("POST")
+	r.HandleFunc("/users/logout", handler.LogoutHandler).Methods("POST")
+	r.HandleFunc("/users/{id}", handler.GetUserInfoHandler).Methods("GET")
+	r.HandleFunc("/users", handler.ListAllUsersHandler).Methods("GET")
+
+	return r
+}
