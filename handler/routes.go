7f14b282c6de110dfb8a3a1a494267454103f864
Yassine Hadj messaoud <modepadu95@riseup.net>
2023-12-23 18:53:57 +0100

    Create Authorization middleware and use it for routes for admin and users

--- handler/routes.go
+++ handler/routes.go
@@ -2,43 +2,61 @@ package handler
 
 import (
 	"github.com/gorilla/mux"
+	"github.com/sandlayth/supplier-api/helper"
 )
 
 func AddUserRoutes(r *mux.Router, handler *UserHandler) {
-	r.HandleFunc("/users", handler.CreateUserHandler).Methods("POST")
-	r.HandleFunc("/users/{id}", handler.GetUserHandler).Methods("GET")
-	r.HandleFunc("/users/{id}", handler.UpdateUserHandler).Methods("PUT")
-	r.HandleFunc("/users/{id}", handler.DeleteUserHandler).Methods("DELETE")
-	r.HandleFunc("/users", handler.ListUsersHandler).Methods("GET")
-    r.HandleFunc("/users/login", handler.LoginHandler).Methods("POST")
-    r.HandleFunc("/users/{id}/renew-token", handler.RenewTokenHandler).Methods("POST")
-//	r.HandleFunc("/logout", handler.LogoutHandler).Methods("POST")
+	r.HandleFunc("/users/login", handler.LoginHandler).Methods("POST")
+	r.HandleFunc("/users/{id}/renew-token", handler.RenewTokenHandler).Methods("POST")
+	// Admin Routes
+	adminRouter := r.PathPrefix("/users").Subrouter()
+	adminRouter.Use(helper.AdminAuthorizationMiddleware)
+	adminRouter.HandleFunc("", handler.CreateUserHandler).Methods("POST")
+	adminRouter.HandleFunc("/{id}", handler.UpdateUserHandler).Methods("PUT")
+	adminRouter.HandleFunc("/{id}", handler.DeleteUserHandler).Methods("DELETE")
+	adminRouter.HandleFunc("", handler.ListUsersHandler).Methods("GET")
+	adminRouter.HandleFunc("/{id}", handler.GetUserHandler).Methods("GET")
+	//	r.HandleFunc("/logout", handler.LogoutHandler).Methods("POST")
 }
 
 func AddLocationRoutes(r *mux.Router, handler *LocationHandler) {
-	//r.HandleFunc("/locations/supplier/{supplierID}", handler.GetAllLocationsForSupplierHandler).Methods("GET")
-	r.HandleFunc("/locations", handler.CreateLocationHandler).Methods("POST")
-	r.HandleFunc("/locations/{id}", handler.GetLocationByIDHandler).Methods("GET")
-	r.HandleFunc("/locations/{id}", handler.UpdateLocationHandler).Methods("PUT")
-	r.HandleFunc("/locations/{id}", handler.DeleteLocationHandler).Methods("DELETE")
-	r.HandleFunc("/locations", handler.ListAllLocationsHandler).Methods("GET")
-	r.HandleFunc("/locations/supplier/{id}", handler.ListBySupplierHandler).Methods("GET")
+	adminRouter := r.PathPrefix("/locations").Subrouter()
+	adminRouter.Use(helper.AdminAuthorizationMiddleware)
+	adminRouter.HandleFunc("/{id}", handler.UpdateLocationHandler).Methods("PUT")
+	adminRouter.HandleFunc("/{id}", handler.DeleteLocationHandler).Methods("DELETE")
+	adminRouter.HandleFunc("", handler.CreateLocationHandler).Methods("POST")
+
+	managerRouter := r.PathPrefix("/locations").Subrouter()
+	managerRouter.Use(helper.ManagerAuthorizationMiddleware)
+	managerRouter.HandleFunc("/{id}", handler.GetLocationByIDHandler).Methods("GET")
+	managerRouter.HandleFunc("", handler.ListAllLocationsHandler).Methods("GET")
+	managerRouter.HandleFunc("/supplier/{id}", handler.ListBySupplierHandler).Methods("GET")
 }
 
 func AddSupplierRoutes(r *mux.Router, handler *SupplierHandler) {
-	r.HandleFunc("/suppliers/{id}", handler.GetSupplierByIDHandler).Methods("GET")
-	r.HandleFunc("/suppliers", handler.GetAllSuppliersHandler).Methods("GET")
-	r.HandleFunc("/suppliers", handler.CreateSupplierHandler).Methods("POST")
-	r.HandleFunc("/suppliers/{id}", handler.UpdateSupplierHandler).Methods("PUT")
-	r.HandleFunc("/suppliers/{id}", handler.DeleteSupplierHandler).Methods("DELETE")
+	adminRouter := r.PathPrefix("/suppliers").Subrouter()
+	adminRouter.Use(helper.AdminAuthorizationMiddleware)
+	adminRouter.HandleFunc("", handler.CreateSupplierHandler).Methods("POST")
+	adminRouter.HandleFunc("/{id}", handler.UpdateSupplierHandler).Methods("PUT")
+	adminRouter.HandleFunc("/{id}", handler.DeleteSupplierHandler).Methods("DELETE")
+
+	managerRouter := r.PathPrefix("/suppliers").Subrouter()
+	managerRouter.Use(helper.ManagerAuthorizationMiddleware)
+	managerRouter.HandleFunc("/{id}", handler.GetSupplierByIDHandler).Methods("GET")
+	managerRouter.HandleFunc("", handler.GetAllSuppliersHandler).Methods("GET")
 }
 
 // AddPurchaseRoutes adds purchase-related routes to the provided router.
 func AddPurchaseRoutes(r *mux.Router, handler *PurchaseHandler) {
-	r.HandleFunc("/purchases", handler.CreatePurchaseHandler).Methods("POST")
-	r.HandleFunc("/purchases/{id}", handler.GetPurchaseHandler).Methods("GET")
-	r.HandleFunc("/purchases/{id}", handler.UpdatePurchaseHandler).Methods("PUT")
-	r.HandleFunc("/purchases/{id}", handler.DeletePurchaseHandler).Methods("DELETE")
-	r.HandleFunc("/purchases", handler.ListAllPurchasesHandler).Methods("GET")
-	r.HandleFunc("/purchases/user/{userID}", handler.ListPurchasesByUserHandler).Methods("GET")
+	adminRouter := r.PathPrefix("/purchases").Subrouter()
+	adminRouter.Use(helper.AdminAuthorizationMiddleware)
+	adminRouter.HandleFunc("/{id}", handler.UpdatePurchaseHandler).Methods("PUT")
+	adminRouter.HandleFunc("/{id}", handler.DeletePurchaseHandler).Methods("DELETE")
+	adminRouter.HandleFunc("/user/{userID}", handler.ListPurchasesByUserHandler).Methods("GET")
+
+	managerRouter := r.PathPrefix("/purchases").Subrouter()
+	managerRouter.Use(helper.ManagerAuthorizationMiddleware)
+	managerRouter.HandleFunc("", handler.CreatePurchaseHandler).Methods("POST")
+	managerRouter.HandleFunc("/{id}", handler.GetPurchaseHandler).Methods("GET")
+	managerRouter.HandleFunc("", handler.ListAllPurchasesHandler).Methods("GET")
 }
--- helper/response.go
+++ helper/response.go
@@ -3,10 +3,72 @@ package helper
 import (
 	"encoding/json"
 	"net/http"
+	"strings"
 )
+
+func AdminAuthorizationMiddleware(next http.Handler) http.Handler {
+	return authorizationMiddleware(next, "admin")
+}
+
+func ManagerAuthorizationMiddleware(next http.Handler) http.Handler {
+	return authorizationMiddleware(next, "manager")
+}
+
+func authorizationMiddleware(next http.Handler, role string) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		// Extract the token from the Authorization header
+		tokenString := extractTokenFromHeader(r)
+		if tokenString == "" {
+			// Token is missing, respond with an authentication error
+			http.Error(w, "Unauthorized", http.StatusUnauthorized)
+			return
+		}
+
+		// Verify the token
+		claims, _, err := VerifyToken(tokenString)
+		if err != nil {
+			// Token is invalid, respond with an authentication error
+			http.Error(w, "Unauthorized", http.StatusUnauthorized)
+			return
+		}
+
+		if !isRoleAllowedToAccess(claims.Role, role) {
+			http.Error(w, "Unauthorized", http.StatusUnauthorized)
+			return
+		}
+
+		// Token is valid, proceed to the next handler
+		next.ServeHTTP(w, r)
+	})
+}
 
 // respondJSON is a helper function to respond with JSON data.
 func RespondJSON(w http.ResponseWriter, data interface{}) {
 	w.Header().Set("Content-Type", "application/json")
 	json.NewEncoder(w).Encode(data)
+}
+
+func isRoleAllowedToAccess(role string, targetRole string) bool {
+	if role == "admin" {
+		return true
+	}
+	if role == "manager" && targetRole == "manager" {
+		return true
+	}
+	return false
+}
+
+func extractTokenFromHeader(r *http.Request) string {
+	authorizationHeader := r.Header.Get("Authorization")
+	if authorizationHeader == "" {
+		return ""
+	}
+
+	// Token is typically included in the format "Bearer <token>"
+	splitHeader := strings.Split(authorizationHeader, " ")
+	if len(splitHeader) != 2 || strings.ToLower(splitHeader[0]) != "bearer" {
+		return ""
+	}
+
+	return splitHeader[1]
 }
